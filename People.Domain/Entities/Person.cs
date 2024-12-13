a06106b0bd27d2df0239081e2a7aa549dcedf2ac
Ben Matela <matelaben2@gmail.com>
2024-12-13 09:47:51 +0200

    init

---
+++ People.Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+namespace Person.Domain.Entities;
+
+/// <summary>
+/// Person Entity
+/// </summary>
+public class Person
+{
+    public Guid Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int Age { get; set; }
+    public DateTime DateCreated { get; set; }
+}
