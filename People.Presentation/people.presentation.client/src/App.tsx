8c673fb98646d99a4e755c143f7fc7419bbe4cb5
Ben Matela <matelaben2@gmail.com>
2024-12-14 08:09:49 +0200

    init routes

--- People.Presentation/people.presentation.client/src/App.tsx
+++ People.Presentation/people.presentation.client/src/App.tsx
@@ -1,17 +1,26 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/home/Home";
+import People from "./pages/people/People";
+import { PageNotFound } from "./pages/page-not-found/PageNotFound";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "*",
+      element: <PageNotFound />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "/people",
+      element: <People />,
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
