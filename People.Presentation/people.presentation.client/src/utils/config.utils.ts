412160e0c4b81caa6f12698bca14338d5a7ef95c
Ben Matela <matelaben2@gmail.com>
2024-12-14 16:45:59 +0200

    clear css

--- People.Presentation/people.presentation.client/src/utils/config.utils.ts
+++ People.Presentation/people.presentation.client/src/utils/config.utils.ts
@@ -1,5 +1,7 @@
-const NODE_ENVIRONMENT = import.meta.env.VITE_NODE_ENV as string;
+const VITE_NODE_ENVIRONMENT = import.meta.env.VITE_NODE_ENV as string;
 const VITE_PEOPLE_API_ENDPOINT = import.meta.env.VITE_PEOPLE_API_ENDPOINT as string;
+const VITE_PORT = parseInt(import.meta.env.VITE_PORT as string);
 
-export const nodeEnv = NODE_ENVIRONMENT;
+export const nodeEnv = VITE_NODE_ENVIRONMENT;
+export const port = VITE_PORT;
 export const peopleApiBaseUrl = VITE_PEOPLE_API_ENDPOINT;
