4a49cf93454de9fc1f8fa62b9aeca0d3705ed14c
Ben Matela <matelaben2@gmail.com>
2024-12-15 13:18:49 +0200

    init react form

--- People.Presentation/people.presentation.client/src/components/forms/reusable-form/ReusableForm.tsx
+++ People.Presentation/people.presentation.client/src/components/forms/reusable-form/ReusableForm.tsx
@@ -18,6 +18,17 @@ interface ReusableFormProps {
   onSubmit: SubmitHandler<FieldValues>;
 }
 
+const useStyles: any = () => ({
+  root: {
+    "& .MuiTextField-root": {
+      width: "100%",
+    },
+    "& .MuiOutlinedInput-root": {
+      borderRadius: "12px",
+    },
+  },
+});
+
 /**
  * Reusable form using react hooks form
  *
@@ -35,50 +46,48 @@ export const ReusableForm: React.FC<ReusableFormProps> = ({
     handleSubmit,
     formState: { errors },
   } = useForm<FieldValues>();
+  const classes: any = useStyles;
 
   return (
-    <Grid2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid2
-          container
-          direction="row"
-          justifyContent={"space-evenly"}
-          style={{ width: "100%" }}
-        >
+    <>
+      <form
+        className={classes.root}
+        onSubmit={handleSubmit(onSubmit)}
+        autoComplete="off"
+      >
+        <Grid2 container spacing={3}>
           {fields.map((field) => (
             <Grid2 key={field.name}>
               <Grid2>
                 <FormLabel htmlFor={field.name}>{field.label}</FormLabel>
               </Grid2>
+              <TextField
+                id={field.name}
+                type={field.type}
+                placeholder={field.placeholder}
+                {...register(field.name, field.validation)}
+                style={{ minWidth: "" }}
+              />
               <Grid2>
-                <TextField
-                  id={field.name}
-                  type={field.type}
-                  placeholder={field.placeholder}
-                  {...register(field.name, field.validation)}
-                  style={{ minWidth: "" }}
-                />
-                <Grid2>
-                  {errors[field.name] && (
-                    <p>{String(errors[field.name]?.message)}</p>
-                  )}
-                </Grid2>
+                {errors[field.name] && (
+                  <p>{String(errors[field.name]?.message)}</p>
+                )}
               </Grid2>
             </Grid2>
           ))}
-          <Grid2>
-            {isLoading ? (
-              <div className="">
-                <CircleLoader size={100} color="#2563eb" />
-              </div>
-            ) : (
-              <Button variant="contained" type="submit" disabled={isLoading}>
-                Submit
-              </Button>
-            )}
-          </Grid2>
+        </Grid2>
+        <Grid2>
+          {isLoading ? (
+            <div className="">
+              <CircleLoader size={100} color="#2563eb" />
+            </div>
+          ) : (
+            <Button variant="contained" type="submit" disabled={isLoading}>
+              Submit
+            </Button>
+          )}
         </Grid2>
       </form>
-    </Grid2>
+    </>
   );
 };
