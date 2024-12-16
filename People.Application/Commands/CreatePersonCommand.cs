4589d9f688e068fb756160abac8a21852de7a7fb
Ben Matela <matelaben2@gmail.com>
2024-12-16 17:20:59 +0200

    init log

--- People.Application/Commands/CreatePersonCommand.cs
+++ People.Application/Commands/CreatePersonCommand.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.Extensions.Logging;
 using People.Application.DTOs;
 using People.Application.Interfaces;
 
@@ -6,14 +7,29 @@ namespace People.Application.Commands;
 
 public record CreatePersonCommand(CreatePersonRequest Person) : IRequest<PersonResponse>;
 
+
 /// <summary>
 /// Handles the command to create a new person.
 /// </summary>
-public class CreatePersonCommandHandler(IPersonRepository personRepository)
+public class CreatePersonCommandHandler(IPersonRepository personRepository, ILogger<CreatePersonCommand> _logger)
     : IRequestHandler<CreatePersonCommand, PersonResponse>
 {
     public async Task<PersonResponse> Handle(CreatePersonCommand request, CancellationToken cancellationToken)
     {
-        return await personRepository.Create(request.Person);
+        try
+        {
+            _logger.LogWarning($"Attempt to create a person: {request}");
+
+            var newPerson = await personRepository.Create(request.Person);
+
+            _logger.LogInformation($"Person created successfully: {newPerson.Id}");
+
+            return newPerson;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while creating the person.");
+            throw; // Rethrow the exception after logging
+        }
     }
 }
--- People.Application/Commands/RemovePersonCommand.cs
+++ People.Application/Commands/RemovePersonCommand.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.Extensions.Logging;
 using People.Application.Interfaces;
 
 namespace People.Application.Commands;
@@ -8,11 +9,31 @@ public record RemovePersonCommand(Guid PersonId) : IRequest<bool>;
 /// <summary>
 /// Handles the command to remove/delete an existing person.
 /// </summary>
-public class RemovePersonCommandHandler(IPersonRepository personRepository)
+public class RemovePersonCommandHandler(IPersonRepository personRepository, ILogger<CreatePersonCommand> _logger)
     : IRequestHandler<RemovePersonCommand, bool>
 {
     public async Task<bool> Handle(RemovePersonCommand request, CancellationToken cancellationToken)
     {
-        return await personRepository.Remove(request.PersonId);
+        try
+        {
+            _logger.LogWarning($"Attempt to delete a person: {request}");
+
+            var deletedPerson = await personRepository.Remove(request.PersonId);
+            if (!deletedPerson)
+            {
+                string errorMessage = $"There was an error while deleting a person: {request.PersonId}";
+                _logger.LogInformation(errorMessage);
+                throw new Exception(errorMessage); // bubbleup the error 
+            }
+;
+            _logger.LogInformation($"Person deleted successfully: {request.PersonId}");
+
+            return deletedPerson;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while deleting the person.");
+            throw; // Rethrow the exception after logging
+        }
     }
 }
